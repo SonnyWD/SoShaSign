e3433cf8b5e9aacbb655e783b2363bc07b59ca6b
Sonny <sonnyterlonwd@gmail.com>
2025-04-21 22:47:10 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx,css}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "./src/css/style.css";`
+      }
+    }
+  }
 });
